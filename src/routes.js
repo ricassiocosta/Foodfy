c99b0a8f63eeb00d6ddc2c7e70a660f6d23793d0
ricassiocosta <riksiocosta@gmail.com>
2020-06-22 14:51:38 -0300

    create recipe button added to admin home

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ routes.get('/sobre', (req, res) => { return res.render('about') })
 routes.get('/receitas', recipesController.index)
 routes.get('/receitas/:recipe_id', recipesController.show)
 
+routes.get('/admin', (req, res) => { return res.redirect('/admin/receitas')})
 routes.get('/admin/receitas', recipesController.adminIndex)
 routes.get('/admin/receitas/criar', recipesController.create)
 routes.get('/admin/receitas/:recipe_id', recipesController.adminShow)
