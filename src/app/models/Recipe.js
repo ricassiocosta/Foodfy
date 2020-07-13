9a774424669b27e55ba99443d8def9583c2e282c
ricassiocosta <riksiocosta@gmail.com>
2020-07-13 15:18:28 -0300

    chef id missing fixed

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -5,16 +5,18 @@ module.exports = {
   create(data, callback) {
     const query = `
       INSERT INTO recipes (
+        chef_id,
         image,
         title,
         ingredients,
         preparation,
         information,
         created_at
-      ) VALUES ( $1, $2, $3, $4, $5, $6 )
+      ) VALUES ( $1, $2, $3, $4, $5, $6, $7 )
       RETURNING ID
     `
     const values = [
+      data.chef,
       data.image,
       data.title,
       [data.ingredients],
