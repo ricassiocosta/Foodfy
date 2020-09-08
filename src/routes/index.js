a12d14c258b69d505071d77a38e742d170e79a8c
ricassiocosta <riksiocosta@gmail.com>
2020-09-08 15:50:46 -0300

    login page created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router()
 const recipesController = require('../app/controllers/recipesController')
 const chefsController = require('../app/controllers/chefsController')
+const sessionController = require('../app/controllers/sessionController')
 const recipes = require('./recipes')
 const chefs = require('./chefs')
 
@@ -9,9 +10,9 @@ routes.get('/sobre', (req, res) => { return res.render('site/about') })
 routes.get('/receitas', recipesController.index)
 routes.get('/chefs', chefsController.listing)
 routes.get('/receitas/:recipe_id', recipesController.show)
+routes.get('/login', sessionController.login)
 
 routes.get('/admin', (req, res) => { return res.redirect('/admin/receitas')})
-
 routes.use('/admin/receitas', recipes)
 routes.use('/admin/chefs', chefs)
 
